0b3dd5a9fb0fedbabb9455ad1a55edeb36a0ee6c
JungWonJoon <shot013@naver.com>
2026-08-22 15:25:08 +0900

    chore(stat): remove non-functional search/notification icons

--- lib/core/widgets/stat_app_bar.dart
+++ lib/core/widgets/stat_app_bar.dart
@@ -31,36 +31,7 @@ class StatAppBar extends StatelessWidget {
             fontStyle: FontStyle.italic,
           ),
         ),
-        const Spacer(),
-        _CircleIconButton(icon: Icons.search, onTap: () {}),
-        const SizedBox(width: 8),
-        _CircleIconButton(icon: Icons.notifications_none, onTap: () {}),
       ],
-    );
-  }
-}
-
-class _CircleIconButton extends StatelessWidget {
-  const _CircleIconButton({required this.icon, required this.onTap});
-
-  final IconData icon;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      customBorder: const CircleBorder(),
-      child: Container(
-        width: 40,
-        height: 40,
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
-          color: Color(0xFF2C2C2E),
-          shape: BoxShape.circle,
-        ),
-        child: Icon(icon, color: Colors.white, size: 20),
-      ),
     );
   }
 }
